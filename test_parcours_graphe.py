6e920908d1fb368da360110be3d69a082a65e13e
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2023-01-13 08:43:29 +0100

    create test file

---
+++ test_parcours_graphe.py
@@ -0,0 +1,66 @@
+from unittest import TestCase
+
+
+class TestNoeud(TestCase):
+    def test_ajouter_enfant(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_parcours_largeur(self):
+        self.fail()
+
+    def test_parcours_profondeur(self):
+        self.fail()
+
+
+class TestGraph(TestCase):
+    def test_add(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
+
+    def test_get_initial(self):
+        self.fail()
+
+
+class TestTransitionRelation(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+
+class TestDictGraph(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+
+class TestNBits(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+
+class TestHanoiConfiguration(TestCase):
+    pass
+
+
+class TestHanoi(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_bfs(self):
+        self.fail()
