e985d516ae3c0917b98caa525e528e7367518ad2
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2023-01-13 09:02:51 +0100

    update test file

---
+++ test.py
@@ -0,0 +1,94 @@
+from unittest import TestCase
+
+
+#__GRAPH_______________________________________________________________________________
+
+class TestNoeud(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_str(self):
+        self.fail()
+
+    def test_repr(self):
+        self.fail()
+
+    def test_ajouter_enfant(self):
+        self.fail()
+
+class TestGraph(TestCase):
+    def test_add(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
+
+    def test_get_initial(self):
+        self.fail()
+
+class TestTransitionRelation(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+class TestDictGraph(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+class TestGraphFunction(TestCase):
+    def test_parcours_largeur(self):
+        self.fail()
+
+    def test_parcours_profondeur(self):
+        self.fail()
+
+    def test_bfs(self):
+        self.fail()
+
+
+
+#__NBITS_______________________________________________________________________________
+
+class TestNBits(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
+
+
+
+#__HANOI_______________________________________________________________________________
+class TestHanoiConfiguration(TestCase):
+    def test_init(self):
+        self.fail()
+
+    def test_hash(self):
+        self.fail()
+
+    def test_str(self):
+        self.fail()
+
+    def test_repr(self):
+        self.fail()
+
+    def test_eq(self):
+        self.fail()
+
+
+class TestHanoi(TestCase):
+    def test_get_roots(self):
+        self.fail()
+
+    def test_next(self):
+        self.fail()
+
