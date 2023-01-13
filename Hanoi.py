b9ee4f78f43a941ade1a914afc41a04315b43330
EmileBrt <emile.berteloot@ensta-bretagne.org>
2023-01-13 08:41:33 +0100

    Refactor

---
+++ Hanoi.py
@@ -0,0 +1,42 @@
+from Graph import TransitionRelation
+
+
+class HanoiConfiguration():
+    def __init__(self, d):
+        self.d = d
+        for l in d.values():
+            assert all(l[i] > l[i + 1] for i in range(len(l) - 1))
+
+    def __hash__(self):
+        return hash(frozenset(self.d))
+
+    def __str__(self):
+        return str(self.d)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other_config):
+        return self.d == other_config.d
+
+class Hanoi(TransitionRelation):
+    def __init__(self, roots):
+        self.roots = roots
+
+    def get_roots(self):
+        print(self.roots)
+        return self.roots
+
+    def next(self, source):
+        childs = []
+        for old in source.d:
+            if len(source.d[old]) == 0:
+                continue
+            for new in source.d:
+                if new == old:
+                    continue
+                if len(source.d[new]) == 0 or source.d[new][-1] > source.d[old][-1]:
+                    child = HanoiConfiguration({x: [y for y in source.d[x]] for x in source.d})
+                    child.d[new].append(child.d[old].pop())
+                    childs.append(child)
+        return childs
---
+++ NBits.py
@@ -0,0 +1,23 @@
+from Graph import TransitionRelation
+
+
+class NBits(TransitionRelation):
+    def __init__(self, roots, n):
+        self.roots = roots
+        self.n = n
+
+    def get_roots(self):
+        return self.roots
+
+    def next(self, source):
+        neighbours = []
+        for i in range(self.n):
+            neighbours.append(source ^ (1 << i))
+
+            """
+            if source >> i & 1 :
+                child = source & ~(1<<i)
+            else :
+                child = source | (1<<i)
+            """
+        return neighbours
