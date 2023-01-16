5cc1f03d7fc1b47db6eeb09ab28a6f77afd9f8ad
EmileBrt <emile.berteloot@ensta-bretagne.org>
2023-01-16 16:08:26 +0100

    Contraint Type on __init__ Rule

--- semantic.py
+++ semantic.py
@@ -1,45 +1,57 @@
 from abc import abstractmethod, ABC
-
+from builtins import *
 
 class SemanticTransitionRelation(ABC):
     @abstractmethod
     def initial_configurations(self): pass
-    
+
     @abstractmethod
-    def enabled_actions(self, source) : pass
-    
+    def enabled_actions(self, source): pass
+
     @abstractmethod
-    def execute(self, action, source) : pass
-    
+    def execute(self, action, source): pass
+
+
 class Rule():
-    def __init__(self, name, guard, action):
+    def __init__(self, name, guard , action):
+        assert isinstance(guard, type(lambda x : x)), f"Expected x to be of type Function but got {type(guard)}"
+        assert isinstance(action, type(lambda x : x)), f"Expected x to be of type Function but got {type(action)}"
+
         self.name = name
         self.guard = guard
         self.action = action
-    
+
     def execute(self, config):
         return [self.action(config)]
-    
+
+
 class SoupConfig():
     pass
+
 
 class SoupProgram():
     def __init__(self, init):
         self.init = init
         self.rules = []
-    def add(self, rule) : 
+
+    def add(self, rule):
         self.rules.append(rule)
-    
+
+
 class SoupSemantic(SemanticTransitionRelation):
     def __init__(self, program):
         self.program = program
-        
+
     def initial_configurations(self):
         return [self.program.init]
-    
+
     def enabled_actions(self, source):
-        return filter(lambda rule : rule.guard(source), self.program.rules)
-    
+        return filter(lambda rule: rule.guard(source), self.program.rules)
+
     def execute(self, rule, source):
         new_source = source.copy()
         return rule.execute(new_source)
+
+
+if __name__ == '__main__':
+    testRule3 = Rule("test3", lambda x: x, lambda x: x)
