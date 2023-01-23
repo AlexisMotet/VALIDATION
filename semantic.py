b93081c85a99b9778a375fa5e0363b41040f2230
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2023-01-23 16:01:38 +0100

    delete rules

--- semantic.py
+++ semantic.py
@@ -1,6 +1,5 @@
 from abc import abstractmethod, ABC
-
-from model import TransitionRelation
+from model import TransitionRelation, Config
 from copy import copy
 
 class SemanticTransitionRelation(ABC):
@@ -77,4 +76,39 @@ class SoupSemantic(SemanticTransitionRelation):
         new_source = copy(source)
         return [rule.execute(new_source)]
 
+
+class SoupConfig(Config):
+    @abstractmethod
+    def __copy__(self):
+        pass
+
+    @abstractmethod
+    def __eq__(self): pass
+
+    @abstractmethod
+    def __hash__(self): pass
+
+
+class AliceAndBobConfig(SoupConfig):
+    def __init__(self, alice, bob, flagAlice=False, flagBob=False):
+        self.alice = alice
+        self.bob = bob
+        self.flagAlice = flagAlice
+        self.flagBob = flagBob
+
+    def __copy__(self):
+        return AliceAndBobConfig(copy(self.alice),
+                                 copy(self.bob))
+
+    def __str__(self):
+        return "Config : {Alice %s - flag Alice %s - Bob %s - flag Bob %s}" % (self.alice, self.flagAlice, self.bob, self.flagBob)
+
+    def __repr__(self):
+        return "Config : {Alice %s - flag Alice %s - Bob %s - flag Bob %s}" % (self.alice, self.flagAlice, self.bob, self.flagBob)
+
+    def __eq__(self, other):
+        return self.alice == other.alice and self.bob == other.bob
+
+    def __hash__(self):
+        return hash(frozenset([self.alice, self.bob]))
 
