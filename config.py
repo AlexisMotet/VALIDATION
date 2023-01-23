9b9c33ad4f553ff4395332caa5880a15f2197fce
WilliamsiNFINITE <williams.hoarau@ensta-bretagne.org>
2023-01-23 14:40:12 +0100

    add flagAlice and flagBob

---
+++ config.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod, ABC
+from copy import copy
+from model import Config
+
+
+class SoupConfig(Config):
+    @abstractmethod
+    def __copy__(self):
+        pass
+
+    @abstractmethod
+    def __eq__(self): pass
+
+    @abstractmethod
+    def __hash__(self): pass
+
+
+class AliceAndBobConfig(SoupConfig):
+    def __init__(self, alice, bob):
+        self.alice = alice
+        self.bob = bob
+        self.flagAlice = False
+        self.flagBob = False
+
+    def __copy__(self):
+        return AliceAndBobConfig(copy(self.alice),
+                                 copy(self.bob))
+
+    def __str__(self):
+        return "Config : {Alice %s - flag Alice %s - Bob %s - flag Bob %s}" % (self.alice, self.flagAlice, self.bob, self.flagBob)
+
+    def __repr__(self):
+        return "Config : {Alice %s - flag Alice %s - Bob %s - flag Bob %s}" % (self.alice, self.flagAlice, self.bob, self.flagBob)
+
+    def __eq__(self, other):
+        return self.alice == other.alice and self.bob == other.bob
+
+    def __hash__(self):
+        return hash(frozenset([self.alice, self.bob]))
+
